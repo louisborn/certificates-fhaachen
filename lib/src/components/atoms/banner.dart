13b25089399d9b8091ac7cfa40fa326924e2e978
Louis Born <louisborn12@gmail.com>
2021-06-13 20:11:13 +0200

    Added documentation for banner.dart

--- lib/src/components/atoms/banner.dart
+++ lib/src/components/atoms/banner.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_svg/flutter_svg.dart';
 
+/// A banner with the brand color for this application.
+///
 class BuildBanner extends StatelessWidget {
+  /// Create a banner.
+  ///
   BuildBanner({
     this.width = double.infinity,
     this.height = 8.0,
   });
 
+  /// The width of the banner.
+  ///
+  /// Default [width] is [double.infinity] (fill the screen width size).
+  ///
   final double width;
 
+  /// The height of the banner.
+  ///
+  /// Default [height] is 8.0 px.
+  ///
   final double height;
 
-  final SvgPicture svg = SvgPicture.asset('assets/images/banner.svg');
+  /// The banner itself.
+  final SvgPicture svg = SvgPicture.asset(
+    'assets/images/banner.svg',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +37,10 @@ class BuildBanner extends StatelessWidget {
       child: this.svg,
     );
 
-    return result;
+    return Semantics(
+      readOnly: true,
+      hint: 'A banner with the brand colors.',
+      child: result,
+    );
   }
 }
