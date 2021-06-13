e9e501c999d2ea3b8f636a9c22e266d843745a4d
Louis Born <louisborn12@gmail.com>
2021-06-13 16:17:30 +0200

    Added documentation to bottom_navigation_bar.dart

--- lib/src/components/organisms/bottom_navigation_bar.dart
+++ lib/src/components/organisms/bottom_navigation_bar.dart
@@ -5,9 +5,14 @@ import '../../../generated/i18n.dart';
 import '../../../screens.dart';
 import '../../../theme.dart';
 
+/// A bottom navigation for this application.
+///
+/// Is used as the base for the [HomeScreen] and [MoreScreen] with.
+/// the bottom navigation bar to switch between those two screens.
+///
 class BuildBottomNavigationBar extends StatefulWidget {
   /// The route name for this screen.
-  static const String route = '/bottomNavigationBar';
+  static const String route = '/start';
 
   @override
   _BuildBottomNavigationBar createState() => _BuildBottomNavigationBar();
@@ -34,40 +39,55 @@ class _BuildBottomNavigationBar extends State<BuildBottomNavigationBar> {
           },
         );
 
+    /// The app bar for this screen.
     final PreferredSizeWidget appBar = BuildAppBar(
       title: I18n.of(context).homeTitle,
       actions: [
         BuildIconButton(
           icon: Icons.info_outline,
           hint: 'Navigates to the about page',
-          onTap: () => Navigator.pushNamed(context, AboutScreen.route),
+          onTap: () => Navigator.pushNamed(
+            context,
+            AboutScreen.route,
+          ),
         ),
         const SizedBox(width: 16.0),
         BuildIconButton(
           icon: Icons.history,
           hint: 'Navigates to the about history log page',
-          onTap: () => Navigator.pushNamed(context, HistoryLogScreen.route),
+          onTap: () => Navigator.pushNamed(
+            context,
+            HistoryLogScreen.route,
+          ),
         ),
         const SizedBox(width: 16.0),
         BuildIconButton(
           icon: Icons.person_outline,
           hint: 'Navigates to the account page',
-          onTap: () => Navigator.pushNamed(context, AccountScreen.route),
+          onTap: () => Navigator.pushNamed(
+            context,
+            AccountScreen.route,
+          ),
         ),
         const SizedBox(width: 16.0),
       ],
     );
 
+    /// The bottom navigation bar for this screen.
     final Widget bottomNavigationBar = BottomNavigationBar(
       currentIndex: _selectedIndex,
       onTap: _onItemTapped,
       items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.home_outlined),
+          icon: const Icon(
+            Icons.home_outlined,
+          ),
           label: I18n.of(context).homeTitle,
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.more_horiz_outlined),
+          icon: const Icon(
+            Icons.more_horiz_outlined,
+          ),
           label: I18n.of(context).homeMore,
         ),
       ],
@@ -78,12 +98,14 @@ class _BuildBottomNavigationBar extends State<BuildBottomNavigationBar> {
       appBar: appBar,
       bottomNavigationBar: bottomNavigationBar,
       body: Padding(
-        padding: EdgeInsets.only(
+        padding: const EdgeInsets.only(
           left: 8.0,
           top: 24.0,
           right: 8.0,
         ),
-        child: _widgets.elementAt(_selectedIndex),
+        child: _widgets.elementAt(
+          _selectedIndex,
+        ),
       ),
     );
   }
